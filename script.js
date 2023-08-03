39db406d51f0d1b310ea053bc3ae018ad5c95170
Arthur-artijo <arthurkabaza@gmail.com>
2023-08-03 15:55:27 +0200

    les 3 premiers exercices

---
+++ script.js
@@ -0,0 +1,61 @@
+let myAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "$",
+    "*",
+    "/",
+    "-",
+    "+",
+  ];
+  
+  console.log(myAlphabet.length);
+  let planets = ["Bujumbura","Kigali","Kampala","Bruxelles","Berlin"];
+
+  planets.forEach((planet) => {
+
+    console.log(planet);
+  }
+  
+  );
+
+let someDataTypes=["prénom",33,true,["arthur"]];
+
+  
+//   console.log(typeof(someDataTypes[0]));
+
+someDataTypes.forEach((planet) => {
+
+    console.log(typeof(planet));
+  }
+  
+  );
+  for (let i = 0 ;i<someDataTypes.length; i++)
+{
+
+    console.log(i);
+}
+  
