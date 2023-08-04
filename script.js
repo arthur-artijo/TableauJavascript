df8eb92f8148600327b47a8c3d51a979d0531b08
Arthur-artijo <arthurkabaza@gmail.com>
2023-08-04 12:59:52 +0200

    les 6 premiers exercices sans commentaire

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Exercice 1
+
 let myAlphabet = [
     "A",
     "B",
@@ -59,3 +61,85 @@ someDataTypes.forEach((planet) => {
     console.log(i);
 }
   
+let foods =[];
+console.log(foods);
+ 
+foods.push("riz");
+
+// console.log(foods);
+foods.forEach((food)=> {
+
+
+  console.log(food);
+}
+
+);
+
+foods.push(4,true,undefined);
+foods.forEach((food)=> {
+
+
+  console.log(food);
+}
+
+);
+let africanFood =[];
+africanFood = foods.slice();
+africanFood.forEach((food)=> {
+
+  
+  console.log(food);
+})
+;
+
+// exercice 5
+
+
+let countries = ["Burundi","Rwanda","Congo","Tanzanie","ouganda","kenya"];
+
+countries.forEach((country) => {
+
+  let displayTable= country.split("");
+
+  // console.log(country.split(""));
+
+  displayTable.forEach((distable)=>
+  {
+console.log(distable);
+  });
+}
+  );
+  
+//  exercice 6
+
+let values1= ["Apple", 1, false, "2"];
+let values2 = [`5`, "Fries", 2 , true];
+let values3 = ["Mars", "Strawberry", 9];
+
+// let newValues1 = values1.filter(element => typeof element == 'number');
+// console.log(newValues1);
+
+let newValues = values1.concat(values2,values3);
+// newValues.forEach((value) => {
+
+// console.log(value);
+// });
+
+for (let i = 0; i<newValues.length;i++)
+{   
+if (typeof(newValues[i])  == 'number')
+{
+console.log(newValues[i]);
+}
+}
+//  // console.log(i);
+
+//  if (values1.filter(i)== Number){
+//   console.log(values1[i]);
+//   }
+//   }
+
+//Exercice 7
+
+let studentCoursesA = ['Math', 'English', 'Programming'];
+let studentCoursesB = ['Geography', 'Spanish', 'Programming'];
