379103de3f36e376fb4cc473ca089b068035bfc6
Arthur-artijo <arthurkabaza@gmail.com>
2023-08-08 17:32:01 +0200

    les 6 premiers exercices corrigés

--- script.js
+++ script.js
@@ -33,21 +33,39 @@ let myAlphabet = [
     "-",
     "+",
   ];
-  
-  console.log(myAlphabet.length);
-  let planets = ["Bujumbura","Kigali","Kampala","Bruxelles","Berlin"];
+  // What is the length of the array?
+   console.log(myAlphabet.length);
 
-  planets.forEach((planet) => {
+   
+ 
+  //  2. Create an Array
+  // Declare and initialize an array called planets with 5 string values.
+   let planets = ["Mercure","Vénus"," Terre","Mars","Jupiter"];
+  //  console.log each item in the array.
+ 
+   planets.forEach((planet) => {
 
     console.log(planet);
   }
   
   );
 
-let someDataTypes=["prénom",33,true,["arthur"]];
+  // Also console.log the index in each iteration.
 
+  for (let i = 0 ;i<planets.length; i++)
+{
   
-//   console.log(typeof(someDataTypes[0]));
+     console.log(i);
+ }
+    
+
+  // 3. Manipulate data types
+
+  // Declare and initialize an array called someDataTypes.
+ let someDataTypes=["prénom",33,true,undefined];
+
+//  Write and display in your console the typeof for each iteration.
+
 
 someDataTypes.forEach((planet) => {
 
@@ -55,53 +73,41 @@ someDataTypes.forEach((planet) => {
   }
   
   );
+  // Display in your console the index of each data in your array.
   for (let i = 0 ;i<someDataTypes.length; i++)
 {
 
     console.log(i);
 }
   
+// 4. Empty Array
+// Create an empty Array.
 let foods =[];
+// console.log this empty Array and keep it during all this exercise.
 console.log(foods);
- 
-foods.push("riz");
-
-// console.log(foods);
-foods.forEach((food)=> {
-
-
-  console.log(food);
-}
-
-);
-
+// Add some data in it, the type you want, the theme you want.
 foods.push(4,true,undefined);
-foods.forEach((food)=> {
-
-
-  console.log(food);
-}
-
-);
+// Copy your Array
 let africanFood =[];
 africanFood = foods.slice();
+// console.log this new Array
 africanFood.forEach((food)=> {
-
-  
   console.log(food);
-})
-;
-
-// exercice 5
+  }
+  
+  );
+ 
+//5. How many letters?
 
 
 let countries = ["Burundi","Rwanda","Congo","Tanzanie","ouganda","kenya"];
+let furnitures = ['Table', 'Chairs','Couch'];
+// For each item in this array console.log the letters in each item.
+furnitures.forEach((furniture) => {
 
-countries.forEach((country) => {
+  let displayTable= furniture.split("");
 
-  let displayTable= country.split("");
-
-  // console.log(country.split(""));
+  console.log(furniture.split(""));
 
   displayTable.forEach((distable)=>
   {
@@ -110,7 +116,7 @@ console.log(distable);
 }
   );
   
-//  exercice 6
+// // 6.Which one is a number?
 
 let values1= ["Apple", 1, false, "2"];
 let values2 = [`5`, "Fries", 2 , true];
@@ -120,10 +126,10 @@ let values3 = ["Mars", "Strawberry", 9];
 // console.log(newValues1);
 
 let newValues = values1.concat(values2,values3);
-// newValues.forEach((value) => {
+newValues.forEach((value) => {
 
-// console.log(value);
-// });
+console.log(value);
+});
 
 for (let i = 0; i<newValues.length;i++)
 {   
@@ -132,14 +138,14 @@ if (typeof(newValues[i])  == 'number')
 console.log(newValues[i]);
 }
 }
-//  // console.log(i);
+ // console.log(i);
 
-//  if (values1.filter(i)== Number){
-//   console.log(values1[i]);
-//   }
-//   }
+ if (values1.filter(i)== Number){
+  console.log(values1[i]);
+  }
+  
 
-//Exercice 7
+// //Exercice 7
 
-let studentCoursesA = ['Math', 'English', 'Programming'];
-let studentCoursesB = ['Geography', 'Spanish', 'Programming'];
+// let studentCoursesA = ['Math', 'English', 'Programming'];
+// let studentCoursesB = ['Geography', 'Spanish', 'Programming'];
